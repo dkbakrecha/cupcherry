ad12fd9c555d2a9276090cfc4a9c3381a74c20b9
Jai Soni <jayksonii@gmail.com>
2015-03-15 14:54:13 +0530

    some minor changes

---
+++ app/Model/TeacherProfile.php
@@ -0,0 +1,82 @@
+<?php
+
+App::uses('AppModel', 'Model');
+App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
+
+class User extends AppModel {
+
+    var $name = 'User';
+    public $validate = array(
+        'username' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A username is required'
+            ),
+            'unique name' => array(
+                'rule' => 'isUnique',
+                'message' => 'this username already taken'
+            )
+        ),
+        'email' => array(
+            'valid email' => array(
+                'rule' => array('email', false), // second parameter verify that the host for the address is valid / I set this false right now
+                'message' => 'Please enter a valid email address'
+            ),
+            'unique email' => array(
+                'rule' => 'isUnique',
+                'message' => 'This email address already registered.'
+            )
+        ),
+        'password' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A password is required'
+            ),
+            'password match' => array(
+                'rule' => 'passwordMatch',
+                'message' => ''
+            )
+        ),
+        'confirm_password' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A confirm password is required'
+            )
+            
+        ),
+        'role' => array(
+            'valid' => array(
+                'rule' => array('inList', array('admin', 'member')),
+                'message' => 'Please enter a valid role',
+                'allowEmpty' => false
+            )
+        )
+    );
+
+    public function passwordMatch() {
+        if ($this->data[$this->alias]['password'] == $this->data['User']['confirm_password']) {
+            return true;
+        }
+        $this->invalidate('confirm_password', 'Passwords do not match.');
+        return false;
+    }
+
+//    public function validate_passwords() {
+//        return $this->data[$this->alias]['password'] === $this->data[$this->alias]['cpassword'];
+//    }
+
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $passwordHasher = new SimplePasswordHasher();
+            $this->data[$this->alias]['password'] = $passwordHasher->hash(
+                    $this->data[$this->alias]['password']
+            );
+
+            if (!empty($this->data)) {
+                $this->data['User']['dob'] = date('Y-m-d', strtotime($this->data['User']['dob']));
+            }
+        }
+        return true;
+    }
+
+}
